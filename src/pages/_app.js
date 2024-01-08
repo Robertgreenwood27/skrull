cb74a31dfa1a27b4c7631817cbea14868644e4a6
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-01-07 18:50:59 -0700

    removed components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,12 @@
 import { useState } from 'react';
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-import { ThemeContext, themes } from '@/components/theme-context';
+
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState(themes.dark); // Default to dark mode
-
-    const toggleTheme = () => {
-      setTheme(theme === themes.dark ? themes.light : themes.dark);
-    };
 
   return (
-    <div className={theme}>
+  
       <Component {...pageProps} />
-      </div>
+  
   );
 }
