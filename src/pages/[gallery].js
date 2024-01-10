d1154665f73cddbe528fc0e22b3fdd41f902e5d9
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-01-10 15:59:32 -0700

    hopefully one frame per scroll

--- src/pages/[gallery].js
+++ src/pages/[gallery].js
@@ -25,23 +25,21 @@ const GalleryPage = () => {
         const touchDuration = Date.now() - initialTouchTime;
     
         // Adjust these thresholds as needed
-        const zoomThreshold = 50; // Increased threshold for zoom sensitivity
-        const timeThreshold = 300; // milliseconds - Increased for slower reaction
+        const zoomThreshold = 30;
+        const timeThreshold = 150; // milliseconds
     
-        // Debounce the zoom action
         if (Math.abs(deltaY) > zoomThreshold && touchDuration > timeThreshold) {
-            if (!lastZoomTime || (Date.now() - lastZoomTime) > 300) { // Debounce zoom action
-                if (deltaY > 0) {
-                    setZoomLevel(prevZoomLevel => Math.min(prevZoomLevel + 1, maxZoomLevel));
-                } else {
-                    setZoomLevel(prevZoomLevel => Math.max(prevZoomLevel - 1, 0));
-                }
-                lastZoomTime = Date.now(); // Update the last zoom time
+            // Likely a zoom gesture
+            if (deltaY > 0) {
+                // Swiping up, zoom in (previously zoom out)
+                setZoomLevel(prevZoomLevel => Math.min(prevZoomLevel + 1, maxZoomLevel));
+            } else {
+                // Swiping down, zoom out (previously zoom in)
+                setZoomLevel(prevZoomLevel => Math.max(prevZoomLevel - 1, 0));
             }
             event.preventDefault();
         }
     };
-    
     
 
     const handleTouchEnd = () => {
@@ -102,15 +100,24 @@ const GalleryPage = () => {
 
     const handleScrollZoom = (event) => {
         if (selectedImage) {
-            if (event.deltaY > 0) {
-                // Scroll down (finger moves up), zoom out
-                setZoomLevel(prevZoomLevel => (prevZoomLevel > 0 ? prevZoomLevel - 1 : prevZoomLevel));
-            } else {
-                // Scroll up (finger moves down), zoom in
-                setZoomLevel(prevZoomLevel => (prevZoomLevel < maxZoomLevel ? prevZoomLevel + 1 : prevZoomLevel));
-            }
+            // Determine the direction of the scroll
+            const zoomingIn = event.deltaY < 0; // deltaY is negative when scrolling up
+    
+            setZoomLevel(prevZoomLevel => {
+                if (zoomingIn) {
+                    // Scroll up, zoom in
+                    return Math.min(prevZoomLevel + 1, maxZoomLevel);
+                } else {
+                    // Scroll down, zoom out
+                    return Math.max(prevZoomLevel - 1, 0);
+                }
+            });
+    
+            event.preventDefault(); // Prevent the default scroll behavior
         }
     };
+    
+    
     
 
     useEffect(() => {
